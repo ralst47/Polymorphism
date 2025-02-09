aee485c0cc3657f661e7f70bdb5fbc9223a3021c
ralst47 <>
2025-02-09 20:22:31 +0400

    Полиморфизм: for each + указатель на динамический массив в куче + ототображение кон/де-структоров

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,57 +1,49 @@
 ﻿#include <iostream>
-#include <cmath>//<math.h>
 
-using namespace std;
-
-class Shape
-{
+class IFigere {
 public:
-    string color;
-    virtual double calculateArea()
-    {
-        return 0.0;
-    }
+	std::string tetromino = "Заготовка";
+	virtual void draw() { std::cout << "Фигура: " << tetromino << std::endl; } /*= 0;*/
+	IFigere() { std::cout << "__IFigere__" << std::endl; }
+	virtual ~IFigere() { std::cout << "__~IFigere__" << std::endl; } /*= default;*/
 };
 
-class Circle : public Shape
-{
-    int radius;
-public:
-    Circle(int radius)
-    {
-        this->radius = radius;
-    }
-    double calculateArea() override
-    {
-        double  M_PI = atan(1) * 4;//Число Пи
-        return M_PI * pow(radius, 2);
-    }
+struct Square :IFigere {
+	std::string tetromino = "Квадрат";
+	void draw()override { std::cout << "Фигура0: " << tetromino << std::endl; }
+	Square() { std::cout << "__Square__" << std::endl; }
+	~Square()override { std::cout << "__~Square__" << std::endl; }
 };
 
-class Rectangle : public Shape
-{
-    int width;
-    int height;
-public:
-    Rectangle(int width, int height)
-    {
-        this->width = width;
-        this->height = height;
-    }
-    double calculateArea() override
-    {
-        return width * height;
-    }
+struct Rectangle :IFigere {
+	std::string tetromino = "Прямоугольник";
+	void draw()override { std::cout << "Фигура1: " << tetromino << std::endl; }
+	Rectangle() { std::cout << "__Rectangle__" << std::endl; }
+	~Rectangle()override { std::cout << "__~Rectangle__" << std::endl; }
 };
 
-int main()
-{
-    Shape* shape1 = new Circle(1);
-    Shape* shape2 = new Rectangle(3, 2);
+struct Line :IFigere {
+	std::string tetromino = "Палка";
+	void draw()override { std::cout << "Фигура2: " << tetromino << std::endl; }
+	Line() { std::cout << "__Line__" << std::endl; }
+	~Line()override { std::cout << "__~Line__" << std::endl; }
+};
 
-    cout << shape1->calculateArea() << endl;
-    cout << shape2->calculateArea() << endl;
+int main() {
+	setlocale(LC_ALL, "Russian");
 
-    delete shape1;
-    delete shape2;
+	constexpr size_t FIGURE_SIZE = 3u;
+
+	IFigere* figures[FIGURE_SIZE] = { new(Square),new(Rectangle),new(Line) };
+
+	for (auto figure : figures) {
+		figure->draw();
+	}
+
+	for (auto figure : figures) {
+		delete figure; // Вызвов деструктора
+		figure = nullptr;
+	}
+
+	return 0;
 }
