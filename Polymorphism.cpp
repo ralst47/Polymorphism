539915d5d86021d5dd1ce122c8883c4dd4a589ce
ralst47 <>
2025-02-09 20:59:44 +0400

    Убран мусор

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,43 +1,33 @@
 ﻿#include <iostream>
 
-class IFigere {
+class IFigere { // Абстрактный класс
 public:
-	std::string tetromino = "Заготовка";
-	virtual void draw() { std::cout << "Фигура: " << tetromino << std::endl; } /*= 0;*/
-	IFigere() { std::cout << "__IFigere__" << std::endl; }
-	virtual ~IFigere() { std::cout << "__~IFigere__" << std::endl; } /*= default;*/
+	virtual void draw() = 0; // Чисто виртуальный метод
+	virtual ~IFigere() = default; // Виртуальный стандартный деструктор
 };
 
 struct Square :IFigere {
 	std::string tetromino = "Квадрат";
 	void draw()override { std::cout << "Фигура0: " << tetromino << std::endl; }
-	Square() { std::cout << "__Square__" << std::endl; }
-	~Square()override { std::cout << "__~Square__" << std::endl; }
 };
 
 struct Rectangle :IFigere {
 	std::string tetromino = "Прямоугольник";
 	void draw()override { std::cout << "Фигура1: " << tetromino << std::endl; }
-	Rectangle() { std::cout << "__Rectangle__" << std::endl; }
-	~Rectangle()override { std::cout << "__~Rectangle__" << std::endl; }
 };
 
 struct Line :IFigere {
 	std::string tetromino = "Палка";
 	void draw()override { std::cout << "Фигура2: " << tetromino << std::endl; }
-	Line() { std::cout << "__Line__" << std::endl; }
-	~Line()override { std::cout << "__~Line__" << std::endl; }
 };
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 
-	constexpr size_t FIGURE_SIZE = 3u;
-
-	IFigere* figures[FIGURE_SIZE] = { new(Square),new(Rectangle),new(Line) };
+	IFigere* figures[] = { new(Square),new(Rectangle),new(Line) };
 
 	for (auto figure : figures) {
-		figure->draw();
+		figure->draw(); // вызов полиморфных методов
 	}
 
 	for (auto figure : figures) {
